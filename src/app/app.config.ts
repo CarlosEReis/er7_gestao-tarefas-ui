8b4b399dd693e329ceb98dfbc229e9bf6142b0bd
CarlosEReis <carlos.er7@gmail.com>
2026-05-13 13:39:23 -0300

    build: instala e configura dependencias base
    - primeuix/themes": "^2.0.3",
    - tailwindcss/postcss": "^4.3.0",
    - postcss": "^8.5.14",
    - primeicons": "^7.0.0",
    - primeng": "^21.1.7",
    - tailwindcss": "^4.3.0",
    - tailwindcss-primeui": "^0.6.1",

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,38 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import { definePreset } from '@primeuix/themes';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
+
+const Er7GestaoTarefas = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50:  '{yellow.50}',
+      100: '{yellow.100}',
+      200: '{yellow.200}',
+      300: '{yellow.300}',
+      400: '{yellow.400}',
+      500: '{yellow.500}',
+      600: '{yellow.600}',
+      700: '{yellow.700}',
+      800: '{yellow.800}',
+      900: '{yellow.900}',
+      950: '{yellow.950}',
+    }
+  },
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Er7GestaoTarefas,
+      },
+      ripple: true,
+    })
   ]
 };
