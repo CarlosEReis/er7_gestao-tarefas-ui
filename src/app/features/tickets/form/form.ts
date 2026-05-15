d0170b96d95e887124dbf9b0090e81cdc559a19d
CarlosEReis <carlos.er7@gmail.com>
2026-05-15 13:15:46 -0300

    feat: cria esqueleto do form criacao tickets

---
+++ src/app/features/tickets/form/form.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Dialog } from 'primeng/dialog';
+
+@Component({
+  selector: 'app-form',
+  imports: [ Dialog ],
+  templateUrl: './form.html',
+  styleUrl: './form.scss',
+})
+export class Form {}
